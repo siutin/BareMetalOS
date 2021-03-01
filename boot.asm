34e770630119cdc55837f4ac441cee82650c2e25
siutin <osiutino@gmail.com>
2021-03-02 01:17:42 +0800

    add multiboot header v2

--- boot.asm
+++ boot.asm
@@ -2,6 +2,22 @@
 ; boot.asm
 ; ----------------------------------------------------------------------------
 
+section .multiboot_header
+header_start:
+      dd            0xe85250d6                ; multiboot v2 magic number
+      dd            0                         ; architecture 0
+      dd            header_end - header_start ; header length
+      
+      ; checksum
+      dd            0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+      
+      ; required end tag
+      dw            0 ; type
+      dw            0 ; flags
+      dd            8 ; size
+
+header_end:
+
 [bits 32]
 
       global        _start

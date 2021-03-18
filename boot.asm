357cf7823410126dd520ad0a3b99a74085c4e8b9
siutin <osiutino@gmail.com>
2021-03-18 22:26:44 +0800

    boot.asm - define multiboot constrants

--- boot.asm
+++ boot.asm
@@ -1,15 +1,17 @@
+MULTIBOOT2_HEADER_MAGIC             equ 0xe85250d6
+GRUB_MULTIBOOT_ARCHITECTURE_I386    equ 0x0
+
 section .multiboot_header
 header_start:
-      dd            0xe85250d6                ; multiboot v2 magic number
-      dd            0                         ; architecture 0
-      dd            header_end - header_start ; header length
+      dd            MULTIBOOT2_HEADER_MAGIC               ; multiboot v2 magic number
+      dd            GRUB_MULTIBOOT_ARCHITECTURE_I386      ; architecture 0
+      dd            header_end - header_start             ; header length
       
       ; checksum
-      dd            0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+      dd            0x100000000 - (MULTIBOOT2_HEADER_MAGIC + 0 + (header_end - header_start))
       
       ; required end tag
       dw            0 ; type
       dw            0 ; flags
       dd            8 ; size
-
 header_end:

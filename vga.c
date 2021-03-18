a590b87cc662e63b5b72e40dd608e6cd821561f8
siutin <osiutino@gmail.com>
2021-03-18 23:54:30 +0800

    vga - replace terminal_write* with printf

--- vga.c
+++ vga.c
@@ -40,30 +40,3 @@ void terminal_putchar(char c)
     }
   }
 }
-
-void terminal_write(const char* data, size_t size)
-{
-  for (size_t i = 0; i < size; i++)
-    terminal_putchar(data[i]);
-}
-
-void terminal_writestring(const char* data)
-{
-  terminal_write(data, strlen(data));
-}
-
-const uint16_t digits[] = {
-    u'0', u'1', u'2', u'3', u'4', u'5', u'6', u'7', u'8', u'9', u'a', u'b', u'c', u'd', u'e', u'f'
-};
-
-void terminal_writehex(uint32_t n) {
-    char buffer[9];
-    char *p = buffer;
-
-    for (int i = 7; i >= 0; --i) {
-      uint8_t nibble = (n & (0xf << (i*4))) >> (i*4);
-      *p++ = digits[nibble];
-    }
-    *p = 0;
-    terminal_writestring(buffer);
-}
--- vga.h
+++ vga.h
@@ -46,6 +46,3 @@ extern void terminal_initialize(void);
 void terminal_setcolor(uint8_t color);
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y);
 void terminal_putchar(char c);
-void terminal_write(const char* data, size_t size);
-void terminal_writestring(const char* data);
-void terminal_writehex(uint32_t n);
